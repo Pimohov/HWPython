3e02c4287465780bd30371a10f41cf9c52fe933f
Pimohov <151530151+Pimohov@users.noreply.github.com>
2023-12-04 19:44:55 +0600

    Add files via upload

---
+++ Домашнее задание/HW 19 GUI.py
@@ -0,0 +1,36 @@
+# Задание №1
+# import tkinter as tk
+#
+# def exit():
+#     window.destroy()
+#
+# window = tk.Tk()
+# window.title('Мое приложение')
+# window.geometry('300x200')
+#
+# button = tk.Button(window, text='Выход', command=exit)
+# button.pack()
+#
+# window.mainloop()
+
+# Задание №2
+# import tkinter as tk
+#
+# def profile():
+#     profile_text = """
+#     Имя: Матвей
+#     Возраст: 14
+#     Город: Сарань
+#     """
+#     label.config(text=profile_text)
+#
+# my_profile = tk.Tk()
+# my_profile.title("Анкета о себе:")
+#
+# label = tk.Label(my_profile)
+# label.pack()
+#
+# button = tk.Button(my_profile, text="Показать анкету", command=profile)
+# button.pack()
+#
+# my_profile.mainloop()
