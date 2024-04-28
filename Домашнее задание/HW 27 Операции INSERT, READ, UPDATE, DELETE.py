c8c79fb728fc265f818bc51d622048c20a6253e0
Pimohov <151530151+Pimohov@users.noreply.github.com>
2024-04-28 16:21:01 +0500

    Add files via upload

---
+++ Домашнее задание/HW 27 Операции INSERT, READ, UPDATE, DELETE.py
@@ -0,0 +1,24 @@
+# Задание №1
+# SELECT * FROM `order`;
+
+# Задание №2
+# SELECT DISTINCT category FROM products;
+
+# Задание №3
+# SELECT first_name, last_name FROM clients WHERE balance > 1000;
+
+# Задание №4
+# SELECT * FROM employees ORDER BY salary DESC;
+
+# Задание №5
+# UPDATE employees SET salary = salary * 1.05;
+#
+# Задание №6
+# UPDATE orders SET status = 'Progressing' WHERE delivery_country = 'USA';
+#
+# Задание №7
+# UPDATE products SET price = price * 0.9 WHERE stock < 20;
+#
+# Задание №8
+# UPDATE customers SET status = 'Active' WHERE balance > 5000;
+
